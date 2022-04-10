bed68b09880794c8bb8f03348546bb1b7c7e0848
AnishKumarGarg <anish0192.be21@chitkara.edu.in>
2022-04-10 15:18:31 +0530

    commited

---
+++ math.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+
+int main(){
+     cout << "The max out of 5 and 10 is " << max(5,10) << endl;
+     cout << "The min out of 5 and 10 is " << min(5,10) << endl;
+     cout << "The square root of 49 is " << sqrt(49) << endl;
+     cout << "The nearest round integer of -7.2 is " << round(-7.2) << endl;
+     cout << "The log of 147 is " << log(147) << endl;
+     
+     return 0;
+}
+
